d4a2e6a0f0e8be16d83f9ebc24ced78291d79344
Bruno Marques <brunofgmarques@gmail.com>
2019-07-26 01:12:59 +0100

    success docx to pdf without disk persistance :D

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # oo-tests
+
+## Start server
+```
+/Applications/OpenOffice.app/Contents/MacOS/soffice -norestore -nofirststartwizard -nologo -headless -invisible -accept="socket,host=localhost,port=8100;urp;StarOffice.ServiceManager"
+```
+
+## Run client
+```
+TestOpenOffice.java
+```
--- src/main/java/com/bmp/tests/oo/TestOpenOffice.java
+++ src/main/java/com/bmp/tests/oo/TestOpenOffice.java
@@ -1,34 +1,40 @@
 package com.bmp.tests.oo;
 
 import com.sun.star.beans.PropertyValue;
+import com.sun.star.bridge.UnoUrlResolver;
+import com.sun.star.bridge.XUnoUrlResolver;
 import com.sun.star.comp.helper.Bootstrap;
-import com.sun.star.comp.helper.BootstrapException;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.frame.XStorable;
 import com.sun.star.io.XInputStream;
+import com.sun.star.io.XOutputStream;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiComponentFactory;
 import com.sun.star.lib.uno.adapter.ByteArrayToXInputStreamAdapter;
-import com.sun.star.sdbc.XCloseable;
-import com.sun.star.uno.Exception;
+import com.sun.star.lib.uno.adapter.OutputStreamToXOutputStreamAdapter;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
 
-import java.io.*;
+import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TestOpenOffice {
 
-    public static void main(String[] args) throws BootstrapException, Exception, IOException {
-        XComponentContext xComponentContext = Bootstrap.bootstrap();
-        XMultiComponentFactory xMultiComponentFactory = xComponentContext.getServiceManager();
+    private static final String BASE_PATH = "/Users/brunomarques/IdeaProjects/tests/ootests/src/main/resources/";
+
+    public static void main(String[] args) throws Exception {
+        XComponentContext xComponentContext = Bootstrap.createInitialComponentContext(null);
+        XUnoUrlResolver xUnoUrlResolver = UnoUrlResolver.create(xComponentContext);
+
+        Object initialObject = xUnoUrlResolver.resolve("uno:socket,host=localhost,port=8100;urp;StarOffice.ServiceManager");
+        XMultiComponentFactory xMultiComponentFactory = (XMultiComponentFactory) UnoRuntime.queryInterface(XMultiComponentFactory.class, initialObject);
 
         Object oDesktop = xMultiComponentFactory.createInstanceWithContext("com.sun.star.frame.Desktop", xComponentContext);
         XComponentLoader xComponentLoader = UnoRuntime.queryInterface(com.sun.star.frame.XComponentLoader.class, oDesktop);
 
         ///
-        byte[] array = Files.readAllBytes(Paths.get("classpath:Sample medium docx.docx"));
+        byte[] array = Files.readAllBytes(Paths.get(BASE_PATH + "Sample medium docx.docx"));
         XInputStream xInputStream = new ByteArrayToXInputStreamAdapter(array);
 
         PropertyValue[] conversionProperties = new PropertyValue[2];
@@ -43,18 +49,14 @@ public class TestOpenOffice {
         XComponent xComponent = xComponentLoader.loadComponentFromURL("private:stream", "_blank", 0, conversionProperties);
 
         ////
-
-        OutputStream outputStream = new FileOutputStream("classpath:/test.pdf");
+        XOutputStream outputStream = new OutputStreamToXOutputStreamAdapter(new FileOutputStream(BASE_PATH + "test.pdf"));
 
         conversionProperties[0].Name = "OutputStream";
         conversionProperties[0].Value = outputStream;
         conversionProperties[1].Name = "FilterName";
         conversionProperties[1].Value = "writer_pdf_Export";
 
-        XStorable xStorable = (XStorable) UnoRuntime.queryInterface(XStorable.class,xComponent);
+        XStorable xStorable = (XStorable) UnoRuntime.queryInterface(XStorable.class, xComponent);
         xStorable.storeToURL("private:stream", conversionProperties);
-
-        XCloseable xCloseable = (XCloseable) UnoRuntime.queryInterface(XCloseable.class,xComponent);
-        xCloseable.close();
     }
 }
